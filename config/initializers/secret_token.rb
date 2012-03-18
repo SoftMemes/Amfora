31f16dae6be50ac1d2d359a82ce83c05ad3030e8
SoftMemes <freed@debian.Home>
2012-03-18 02:19:37 +0000

    Adding empty rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amfora::Application.config.secret_token = '26ba56bc20fea928c7a1f0e725cd6bb2f96906b6f05b9f7fce6f50e48ec6c31f654d9ab4a23a758c44ecfb06ecf628735be6c9f89ea415bcd3937063b41672c5'
